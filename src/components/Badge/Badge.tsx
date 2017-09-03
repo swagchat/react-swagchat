dd4598ef1e4621f7b3874d07bea08fc4ef907105
betchi <shinichi.minobe@gmail.com>
2017-09-03 14:34:49 +0900

    Update Badge component

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -4,13 +4,21 @@ const classNames = require('classnames');
 export interface IBackButtonProps {
   count: number;
   className?: string;
+  style?: Object;
 }
 
 export class Badge extends React.Component<IBackButtonProps, {}> {
+  public static defaultProps: Partial<IBackButtonProps> = {
+    className: '',
+    style: {},
+  };
+
   render(): JSX.Element  {
+    const { className, style } = this.props;
     return (
       <span
-        className={classNames('badge', this.props.className)}
+        className={classNames('sc-badge', className)}
+        style={style}
       >
         {this.props.count}
       </span>
