b890303f90962924b837217b4407736f77c18ca3
betchi <shinichi.minobe@gmail.com>
2017-09-04 15:07:34 +0900

    Update CheckListItem component

--- src/components/CheckListItem/CheckListItem.tsx
+++ src/components/CheckListItem/CheckListItem.tsx
@@ -1,38 +1,85 @@
 import * as React from 'react';
-import { IUser } from 'swagchat-sdk';
-import { IOnClickProps } from 'swagchat-sdk';
-import { Avatar, Button, RadioButtonChecked, RadioButtonUnChecked } from '../../components';
+import { Avatar, RadioButtonChecked, RadioButtonUnChecked, IOnClickProps } from '../';
 
 export interface ICheckListItemProps extends IOnClickProps {
   name: string;
   pictureUrl: string;
-  width?: number;
-  height?: number;
-  isChecked: boolean;
+  isChecked?: boolean;
+  width?: string;
+  height?: string;
+  className?: string;
+  style?: Object;
 }
 
 export class CheckListItem extends React.Component<ICheckListItemProps, {}> {
-  onClick(user: IUser) {
-    if (this.props.onClick) {
-      this.props.onClick(user);
+  private _checkedDom: HTMLDivElement | null;
+  private _unCheckedDom: HTMLDivElement | null;
+
+  public static defaultProps: Partial<ICheckListItemProps> = {
+    isChecked: false,
+    className: '',
+    style: {},
+    onClick: () => {},
+  };
+
+  constructor(props: ICheckListItemProps) {
+    super(props);
+  }
+
+  componentDidMount() {
+    if (this._checkedDom && this._unCheckedDom) {
+      this._checkedDom.addEventListener('click', this.onClick.bind(this));
+      this._unCheckedDom.addEventListener('click', this.onClick.bind(this));
     }
   }
 
+  onClick() {
+    if (this._checkedDom && this._unCheckedDom) {
+      let checkDomStyle = this._checkedDom.style;
+      let unCheckedDom = this._unCheckedDom.style;
+      checkDomStyle.display === 'block' ? checkDomStyle.display = 'none' : checkDomStyle.display = 'block';
+      unCheckedDom.display === 'block' ? unCheckedDom.display = 'none' : unCheckedDom.display = 'block';
+      this._checkedDom.setAttribute('style', checkDomStyle.cssText);
+      this._unCheckedDom.setAttribute('style', unCheckedDom.cssText);
+    }
+    this.props.onClick ? this.props.onClick() : null;
+  }
+
   render(): JSX.Element  {
+    const { name, pictureUrl, isChecked, width, height } = this.props;
+
     return (
-      <div className="check-list-item-wrap" onClick={this.props.onClick}>
-        <div className="check-list-item-flex1" style={{height: this.props.width + 'px', lineHeight: this.props.width + 'px'}}>
-          <Avatar src={this.props.pictureUrl} width={this.props.width} height={this.props.height} />
+      <div className="sc-check-list-item-wrap" onClick={this.onClick.bind(this)}>
+        <div className="sc-check-list-item-flex1">
+          <Avatar src={pictureUrl} width={width} height={height} />
         </div>
-        <div className="check-list-item-flex2" style={{height: this.props.width + 'px', lineHeight: this.props.width + 'px'}}>
-          <div className="check-list-item-subject">{this.props.name}</div>
+        <div className="sc-check-list-item-flex2">
+          <div className="sc-check-list-item-subject">{name}</div>
         </div>
-        <div className="check-list-item-flex3" style={{height: this.props.width + 'px', lineHeight: this.props.width + 'px'}}>
+        <div className="sc-check-list-item-flex3">
           {(() => {
-            if (this.props.isChecked) {
-              return <Button icon={<RadioButtonChecked />} />;
+            if (isChecked) {
+              return (
+                <div>
+                  <div ref={(child) => this._checkedDom = child} style={{display: 'block'}}>
+                    <RadioButtonChecked />
+                  </div>
+                  <div ref={(child) => this._unCheckedDom = child} style={{display: 'none'}}>
+                    <RadioButtonUnChecked style={{fill: 'rgba(153, 153, 153, 0.2)'}} />
+                  </div>
+                </div>
+              );
             } else {
-              return <Button icon={<RadioButtonUnChecked style={{fill: 'rgba(153, 153, 153, 0.2)'}} />} />;
+              return (
+                <div>
+                  <div ref={(child) => this._checkedDom = child} style={{display: 'none'}}>
+                    <RadioButtonChecked />
+                  </div>
+                  <div ref={(child) => this._unCheckedDom = child} style={{display: 'block'}}>
+                    <RadioButtonUnChecked style={{fill: 'rgba(153, 153, 153, 0.2)'}} />
+                  </div>
+                </div>
+              );
             }
           })()}
         </div>
